0e3bba2079566d659760dc242762456ac785e817
Ethan <ethanmlowenthal@gmail.com>
2022-05-29 13:43:26 -0700

    update display

--- src/Display.cpp
+++ src/Display.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <sstream>
+
 #include <cairomm/context.h>
 #include <cairomm/surface.h>
 
@@ -5,9 +10,15 @@
 
 class Display {
     FrameBuffer fb {0};
-    
+
     Cairo::RefPtr<Cairo::Context> ctx;
     Cairo::RefPtr<Cairo::ImageSurface> surface;
+
+    std::ostringstream str_out {};
+
+    void display_text() {
+        
+    }
 
     public:
         Display() {
@@ -15,13 +26,53 @@ class Display {
                 fb.fb_info.var.xres, fb.fb_info.var.yres, fb.fb_info.fix.line_length); 
 
             ctx = Cairo::Context::create(surface);
+
+            str_out << std::setprecision(2);
         };
 
-        void update() {
+        template<typename T>
+        Display& operator<<(Display& d, const T& obj)
+        {
+            str_out << obj;
+            std::cout << obj << std::endl;
+            return d;
+        }
+
+        void update(GPS gps) {
             ctx->rectangle(0,0,surface->get_width(),surface->get_height());
-            ctx->set_source_rgb(0, 1, 0);
+            ctx->set_source_rgb(0, 0, 0);
             ctx->fill();
 
+            ctx->select_font_face("serif",
+                Cairo::FontSlant::FONT_SLANT_NORMAL,
+                Cairo::FontWeight::FONT_WEIGHT_BOLD);
+
+            ctx->set_font_size(40);
+
+            ctx->set_source_rgb(1, 1, 1);
+
+            ctx->move_to(20, 40);
+            
+
+            this << "Latitude: " << gps.lat << End;
+            // ctx->show_text(str_out.str());  
+            // str_out.str("");
+            // str_out.clear();
+
+            ctx->move_to(20, 100);
+            str_out << "Longitude: " << gps.lon;
+            ctx->show_text(str_out.str());  
+            ctx->move_to(20, 170);
+            ctx->show_text("Speed: "+std::to_string(gps.speed)+" kts");  
+            ctx->move_to(20, 240);
+            ctx->show_text("CMG: "+std::to_string(gps.heading));
+
+            delta_ticks = clock() - current_ticks; 
+            if(delta_ticks > 0)
+                fps = CLOCKS_PER_SEC / delta_ticks;
+
+            ctx->move_to(surface->get_width()-100, 30);
+            ctx->show_text(std::to_string(fps));  
             fb.flip();
         }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,6 @@
 // sudo apt-get install libcairomm-1.0-dev libgps-dev
 // https://techoverflow.net/2021/10/19/how-to-hide-all-boot-text-blinking-cursor-on-raspberry-pi/
 
-#include <iostream>
-#include <string>
-#include <ctime>
-#include <sstream>
-
 #include <cairomm/context.h>
 #include <cairomm/surface.h>
 
@@ -21,8 +16,8 @@ int main() {
 	// 	fb.fb_info.var.xres, fb.fb_info.var.yres, fb.fb_info.fix.line_length); 
   // auto cr = Cairo::Context::create(surface);
 
-  clock_t current_ticks, delta_ticks;
-  clock_t fps = 0;
+  // clock_t current_ticks, delta_ticks;
+  // clock_t fps = 0;
 
   GPS gps {};
   Display display {};
@@ -31,44 +26,6 @@ int main() {
     current_ticks = clock();
     
     gps.update();
-
-    // cr->rectangle(0,0,surface->get_width(),surface->get_height());
-    // cr->set_source_rgb(0, 0, 0);
-    // cr->fill();
-
-    // cr->select_font_face("serif",
-    //     Cairo::FontSlant::FONT_SLANT_NORMAL,
-    //     Cairo::FontWeight::FONT_WEIGHT_BOLD);
-
-    // cr->set_font_size(40);
-
-    // cr->set_source_rgb(1, 1, 1);
-
-    // cr->move_to(20, 40);
-
-    // std::ostringstream str_out;
-    // str_out << std::setprecision(2);
-
-    // str_out << "Latitude: " << gps.lat;
-    // cr->show_text(str_out.str());  
-    // str_out.str("");
-    // str_out.clear();
-
-    // cr->move_to(20, 100);
-    // str_out << "Longitude: " << gps.lon;
-    // cr->show_text(str_out.str());  
-    // cr->move_to(20, 170);
-    // cr->show_text("Speed: "+std::to_string(gps.speed)+" kts");  
-    // cr->move_to(20, 240);
-    // cr->show_text("CMG: "+std::to_string(gps.heading));
-
-    // delta_ticks = clock() - current_ticks; 
-    // if(delta_ticks > 0)
-    //     fps = CLOCKS_PER_SEC / delta_ticks;
-
-    // cr->move_to(surface->get_width()-100, 30);
-    // cr->show_text(std::to_string(fps));  
-    // fb.flip();
-    display.update();
+    display.update(gps);
   }
 }
