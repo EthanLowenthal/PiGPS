666146fa245e155e18bbea7973dede0f5cc0578d
Ethan <ethanmlowenthal@gmail.com>
2022-08-27 19:07:04 -0700

    test

--- src/IO.cpp
+++ src/IO.cpp
@@ -10,7 +10,7 @@ void IO::update(Data& data)
 {
     if (digitalRead(TIMER_START_PIN) == 1 && !data.timer_started)
     {
-        data.timer_started = true;
+        data.timer_started = false;
         data.timer_start_time = Util::get_current_ms();
     }
 }
